8793b0548ce0d07abe24cbf7b8bd3d837080456f
zeroxragde <ragde360@gmail.com>
2023-08-23 17:40:56 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@ REM Instalar paquetes de Python
 set "packages=pydub youtube_dl beautifulsoup4 google-api-python-client moviepy telebot flask ffmpeg yt-dlp validators pattern DBUtils python-dotenv flask-cors Flask-Caching isodate matplotlib schedule"
 
 for %%p in (%packages%) do (
-    "\venv\Scripts\pip" install %%p
+    "venv\Scripts\pip" install %%p
 )
 pause
