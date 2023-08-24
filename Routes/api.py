88c5db0584280c4db7b669e931b2ee1f229fdab8
zeroxragde <ragde360@gmail.com>
2023-08-23 20:14:40 -0500

    Update api.py

--- Routes/api.py
+++ Routes/api.py
@@ -4,10 +4,11 @@ from Controllers.youtubeplaylist import YoutubePlaylistController
 from Libs.Route import Route
 from Response.ApiRespnse import ApiResponse
 
+
 class api(Route):
 
-    def __init__(self, app,conf):
-        super(api,self).__init__(app, self.__class__.__name__, conf, )
+    def __init__(self, app, conf):
+        super(api, self).__init__(app, self.__class__.__name__, conf, )
 
         # Definir ruta
         @self.route("/")
@@ -18,26 +19,9 @@ class api(Route):
         def version():
             return "ZeroX API version 1.0"
 
-        self.group("auth",[
-            {"endpoint":'login',"controller":AuthController,'func':"login", "method":"POST"},
-            {"endpoint":'logout',"controller":AuthController,'func':"logout", "method":"POST"},
-            {"endpoint":'check',"controller":AuthController,'func':"checktoken", "method":"POST",
-             "middleware":"authMiddleware:tokenCheck"}
+        self.group("auth", [
+            {"endpoint": 'login', "controller": AuthController, 'func': "login", "method": "POST"},
+            {"endpoint": 'logout', "controller": AuthController, 'func': "logout", "method": "POST"},
+            {"endpoint": 'check', "controller": AuthController, 'func': "checktoken", "method": "POST",
+             "middleware": "authMiddleware:tokenCheck"}
         ])
-
-        self.group("youtube",[
-            {"endpoint": 'categorias/<idpais>', "controller": YoutubeController, 'func': "categorias", "method": "GET"},
-            {"endpoint": 'q/<query>', "controller": YoutubeController, 'func': "buscar", "method": "GET",
-             "middleware":"authMiddleware:tokenCheck"},
-            {"endpoint": 'detail/<videoid>', "controller": YoutubeController, 'func': "getVideoData", "method": "GET"},
-            {"endpoint": 'stream/<videoid>', "controller": YoutubeController, 'func': "stream", "method": "GET"},
-            {"endpoint": 'streaming/<videoid>', "controller": YoutubeController, 'func': "streamingVideo", "method": "GET"}
-        ])
-
-        #DEJAR PENDIENTE ESTO
-        self.group("perfil",[
-           {"endpoint": 'listas', "controller": YoutubePlaylistController, 'func': "obtenerListas", "method": "GET",
-            "middleware": "authMiddleware:tokenCheck"},
-        ])
-        # CREAR API PARA CONSULTAR VIDEO DESDE LA VPN
-
